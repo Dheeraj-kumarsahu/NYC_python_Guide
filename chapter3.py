4965d40c582b7f01a3fda59255268bd92f1a0abf
Dheerajkumarsahu <dheerajkumarsahu827@gmail.com>
2026-06-25 15:50:39 +0530

    DATA Types added

--- chapter3.py
+++ chapter3.py
@@ -30,15 +30,26 @@
 
 
 #Print type 
+
+#String
 A=12
+#Float
 B=13.01
+#Boolean
 D=True
+#NoneType
 E= None
+#String
 C="hello"
+#List
 F= [1,2,3,4,5]
+#Set
 G={1,"Hi",7,"Hello"}
+#Tuple
 H= (1,2,3,4,5)
+#Dictionary
 I= {1: "hello", 2: "hi", 3: "how are you"}
+#Complex
 J = 12 + 3j
 
 print(type(A))
