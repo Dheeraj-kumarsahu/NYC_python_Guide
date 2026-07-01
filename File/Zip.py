de06c25fec416022f98c7b06da05aa81fa46046c
Dheerajkumarsahu <dheerajkumarsahu827@gmail.com>
2026-07-01 17:44:05 +0530

    Zip:Added

---
+++ File/Zip.py
@@ -0,0 +1,5 @@
+#zip - Combines 2 list into Pair of list 
+names = ['Alpha', 'Bravo', 'Charlie','Delta']
+ages = [25, 30, 35]
+combined = list(zip(names, ages))
+print(f'Combined list of names and ages:- {combined}')
